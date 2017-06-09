282a1480bba0283242d585c866f2587d056b184b
Josef Dolezal <pepik.ml@gmail.com>
2017-06-09 18:36:32 +0200

    Create protocol describing task

---
+++ Sources/MekosFramework/Models/TaskType.swift
@@ -0,0 +1,24 @@
+//
+//  TaskType.swift
+//  mekos
+//
+//  Created by Josef Dolezal on 08/06/2017.
+//
+//
+
+import Foundation
+
+/// Interface of runnable tasks
+protocol TaskType {
+
+    /// Creates new task instance with given configuration.
+    ///
+    /// - Parameter configuration: Task configuration
+    /// - Throws: MekosError on invalid configuration
+    init(configuration: Any) throws
+
+    /// Runs task
+    ///
+    /// - Throws: MekosError on runtime error
+    func run() throws
+}
