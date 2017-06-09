b7ba883423b93f35b187e5ad52b14945301b091b
Josef Dolezal <pepik.ml@gmail.com>
2017-06-09 18:38:46 +0200

    Add error type for invalid configuration into MekosError

--- Sources/MekosFramework/MekosError.swift
+++ Sources/MekosFramework/MekosError.swift
@@ -11,11 +11,18 @@ import Foundation
 public enum MekosError: Error, CustomStringConvertible {
     case missingTask(name: String)
     case invalidDotfile(file: String)
+    case invalidConfigurationFile
 
     public var description: String {
         switch self {
-        case let .missingTask(name): return "The required task `\(name)` is not presented in configuration file."
-        case let .invalidDotfile(file): return "The given dotfile `\(file)` is not valid."
+        case let .missingTask(name):
+            return "The required task `\(name)` is not presented in configuration file."
+
+        case let .invalidDotfile(file):
+            return "The given dotfile `\(file)` is not valid."
+
+        case .invalidConfigurationFile:
+            return "Given configuration file does not have valid format."
         }
     }
 }
