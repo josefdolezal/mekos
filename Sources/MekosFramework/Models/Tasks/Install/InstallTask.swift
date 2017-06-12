bb974142c9e092e707987eb1437898c502b81934
Josef Dolezal <pepik.ml@gmail.com>
2017-06-12 23:22:03 +0200

    Add basic implementation for brew installer

--- Sources/MekosFramework/Models/Tasks/Install/InstallTask.swift
+++ Sources/MekosFramework/Models/Tasks/Install/InstallTask.swift
@@ -12,11 +12,43 @@ struct InstallTask: TaskType {
     /// Logging service
     var logger: Logger?
 
-    init(configuration: Any) throws {
+    let installables: [String]
 
+    init(configuration: Any) throws {
+        guard let installables = configuration as? [String] else {
+            throw MekosError.invalidConfigurationFile
+        }
+
+        self.installables = installables
     }
 
     func run() throws {
+        for installable in installables {
+            logger?.log(message: "Running brew installation of `\(installable)`.")
 
+            if execShellCommand("/usr/local/bin/brew", arguments: ["install", installable]) {
+                logger?.log(success: "The `\(installable)` successfully installed.")
+            } else {
+                logger?.log(error: "The `\(installable)` could not be installed with brew.")
+            }
+        }
+    }
+
+    private func execShellCommand(_ command: String, arguments: [String]) -> Bool {
+        let task = Process()
+        let pipe = Pipe()
+
+        task.launchPath = command
+        task.arguments = arguments
+        task.standardOutput = pipe
+
+        task.launch()
+        task.waitUntilExit()
+
+        if let output = String(data: pipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8) {
+            logger?.log(message: output)
+        }
+
+        return task.terminationStatus == 0 ? true : false
     }
 }
