17a4578068d123435ff169fd9ef79461c5745961
Josef Dolezal <pepik.ml@gmail.com>
2017-06-16 23:56:53 +0200

    Add docs for AppStoreProvider

--- Sources/MekosFramework/Models/Tasks/Install/AppStoreProvider/AppStoreProvider.swift
+++ Sources/MekosFramework/Models/Tasks/Install/AppStoreProvider/AppStoreProvider.swift
@@ -7,23 +7,28 @@
 
 import Foundation
 
-
+/// AppStore software installation provider.
+/// As official API is not provided by Apple, this provider calls `mas` CLI wrapper
+/// for App Store API under the hood.
 struct AppStoreProvider: ProviderType {
-
-    private static let appStoreCommand = "mas"
-
+    /// Logging service
     var logger: Logger?
 
+    /// Installation commands for `mas`
     private let commands: [ShellCommand]
 
+    /// Creates new instance of App Store provider
+    ///
+    /// - Parameter installables: List of software which should be installed
     init(installables: [String]) {
         // Map app id's to shell install commands
         self.commands = installables.map { installable in
-            return ShellCommand(command: AppStoreProvider.appStoreCommand,
+            return ShellCommand(command: AppStoreCommand.binName,
                                 arguments: [AppStoreCommand.install.rawValue, installable])
         }
     }
 
+    /// Installs software from App Store
     func install() {
         // Install each App Store app separately
         for command in commands {
@@ -37,7 +42,7 @@ struct AppStoreProvider: ProviderType {
                 logger?.log(message: try command.execute())
                 logger?.log(success: "The `\(installable)` successfully installed.")
             } catch {
-                logger?.log(warning: "The `\(installable)` could not be installed with AppStore.")
+                logger?.log(warning: "The `\(installable)` could not be installed with App Store.")
             }
         }
     }
@@ -48,4 +53,7 @@ struct AppStoreProvider: ProviderType {
 /// - install: Installs new package
 enum AppStoreCommand: String {
     case install
+
+    /// AppStore `mas` CLI wrapper binary path
+    static let binName = "mas"
 }
