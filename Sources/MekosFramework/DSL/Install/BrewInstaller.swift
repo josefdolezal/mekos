8858d38ca192a638b302804013a923b861136608
Josef Dolezal <pepik.ml@gmail.com>
2017-11-12 23:01:22 +0100

    Implement brew install action

--- Sources/MekosFramework/DSL/Install/BrewInstaller.swift
+++ Sources/MekosFramework/DSL/Install/BrewInstaller.swift
@@ -10,10 +10,24 @@ import Foundation
 /// Provider of brew software, installs given packages.
 public struct BrewInstaller {
     /// Default brew command name
-    private static let brewCommand = "brew"
+    private static let brew = "brew"
 
     /// Run installation of given commands
     public func install(_ packages: [String]) {
+        try! BrewInstaller.execute(subcommand: "install", arguments: packages)
+    }
 
+    /// Executes brew subcommand with given arguments
+    ///
+    /// - Parameters:
+    ///   - subcommand: Name of brew subcommand (eg. `install`)
+    ///   - arguments: Subcommand arguments (eg. swiftenv)
+    /// - Returns: Command standard output
+    /// - Throws: ShellError
+    @discardableResult
+    private static func execute(subcommand: String, arguments: [String] = []) throws -> String {
+        let command = Shell(command: brew, arguments: arguments)
+
+        return try command.execute()
     }
 }
